8ee2909721d0ee8b5c4ddb96970b8c055e1e76d2
Benjamin Mintz <posters@posters.win>
2017-12-14 04:20:01 +0000

    😉

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ app.jinja_env.lstrip_blocks = True
 
 
 @app.before_request
-def csrf_protect():
+def csrf_protecc():
 	if request.method == 'POST':
 		token = session.pop('_csrf_token', None)
 		if token is None or token != request.form.get('_csrf_token'):
@@ -52,6 +52,7 @@ def generate_csrf_token():
 	if '_csrf_token' not in session:
 		session['_csrf_token'] = token_urlsafe()
 	return session['_csrf_token']
+
 
 app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
